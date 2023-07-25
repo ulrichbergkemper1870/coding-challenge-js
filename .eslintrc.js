4dec6019a9174877a91cc72e60f56332629b9d83
Ulli Bergkemper <54021848+ulrichbergkemper1870@users.noreply.github.com>
2023-07-25 11:14:14 +0000

    initial setup and upgrades

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,15 +7,15 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    "@vue/prettier"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
   },
   overrides: [
     {
