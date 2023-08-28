d3bacf5d0af4a78671bad4fedb65632385d95249
Laura Oppermann <laura.oppermann98@gmx.de>
2023-08-28 23:59:32 +0200

    add table and styles for dicing results

--- src/views/NicerDicer.vue
+++ src/views/NicerDicer.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Nicer Dicer</h1>
 
-  <select @change="selectedDice" v-model="selected">
+  <select @change="resetDice" v-model="selected">
     <option disabled value="">Please select one Dice</option>
     <option>4</option>
     <option>6</option>
@@ -9,27 +9,37 @@
     <option>20</option>
   </select>
   <button @click="rolldice">Roll the Dice</button>
-  <ol id="rolledNumbers">
-    <li v-for="(amount, index) in rolledNumbers" :key="index">
-      <DiceResult
-        :rolled-number="index + 1"
-        :amount="amount"
-        :total-amount="numberOfRolls"
-      />
-    </li>
-  </ol>
+  <button @click="resetDice">Reset all Stats</button>
+
+  <table id="firstTable">
+    <thead>
+      <tr>
+        <th>Number</th>
+        <th>Absolute Amount</th>
+        <th>Relative Percentage to total rolls</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(amount, index) in rolledNumbers" :key="index">
+        <DiceResult
+          :rolled-number="index + 1"
+          :amount="amount"
+          :total-amount="numberOfRolls"
+        />
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-// import rolldice from "../rolldice"
 import DiceResult from "../components/DiceResult.vue";
 
 const selected = ref<number>(4);
 const rolledNumbers = ref<number[]>([0, 0, 0, 0]);
 const numberOfRolls = ref<number>(0);
 
-const selectedDice = () => {
+const resetDice = () => {
   const newNumbers = [];
   for (let i = 0; i < selected.value; i++) {
     newNumbers.push(0);
@@ -47,5 +57,45 @@ const rolldice = () => {
 </script>
 
 <style>
-/* css goes here */
+button {
+  background: #afb4d8;
+  color: #fff;
+  padding: 10px;
+  min-width: 30px;
+  border-radius: 5px;
+  margin: 0px 4px;
+  border: none;
+}
+select,
+input {
+  border: 1px solid #afb4d8;
+  padding: 8px;
+  border-radius: 5px;
+  margin: 4px;
+}
+table {
+  margin: auto;
+  margin-top: 36px;
+}
+table th {
+  text-transform: uppercase;
+  text-align: left;
+  background: #afb4d8;
+  color: #fff;
+  padding: 8px;
+  min-width: 30px;
+  border-radius: 5px;
+}
+
+table td {
+  text-align: left;
+  padding: 8px;
+  border-radius: 5px;
+}
+table td:last-child {
+  border-right: none;
+}
+table tbody tr:nth-child(2n) td {
+  background: #e0e3f7;
+}
 </style>
