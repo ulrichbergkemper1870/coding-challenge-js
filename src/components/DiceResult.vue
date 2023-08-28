13551fce9b845aa782d3a711a46493bd52cdbf24
Laura Oppermann <laura.oppermann98@gmx.de>
2023-08-28 23:20:10 +0200

    add nicerdicer functionality

---
+++ src/components/DiceResult.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ rolledNumber }}, {{ amount }}</div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  rolledNumber: Number,
+  amount: Number,
+  totalAmount: Number,
+});
+</script>
+
+<style>
+/* css goes here */
+</style>
--- src/views/NicerDicer.vue
+++ src/views/NicerDicer.vue
@@ -1,11 +1,51 @@
 <template>
-    <h1>Nicer Dicer</h1>
+  <h1>Nicer Dicer</h1>
+
+  <select @change="selectedDice" v-model="selected">
+    <option disabled value="">Please select one Dice</option>
+    <option>4</option>
+    <option>6</option>
+    <option>12</option>
+    <option>20</option>
+  </select>
+  <button @click="rolldice">Roll the Dice</button>
+  <ol id="rolledNumbers">
+    <li v-for="(amount, index) in rolledNumbers" :key="index">
+      <DiceResult
+        :rolled-number="index + 1"
+        :amount="amount"
+        :total-amount="numberOfRolls"
+      />
+    </li>
+  </ol>
 </template>
 
 <script setup lang="ts">
-  // script goes here
+import { ref } from "vue";
+// import rolldice from "../rolldice"
+import DiceResult from "../components/DiceResult.vue";
+
+const selected = ref<number>(4);
+const rolledNumbers = ref<number[]>([0, 0, 0, 0]);
+const numberOfRolls = ref<number>(0);
+
+const selectedDice = () => {
+  const newNumbers = [];
+  for (let i = 0; i < selected.value; i++) {
+    newNumbers.push(0);
+  }
+  rolledNumbers.value = newNumbers;
+  numberOfRolls.value = 0;
+};
+
+const rolldice = () => {
+  // creates number between 0 and N-1 (N = number of sides at the dice)
+  const newRoll = Math.floor(Math.random() * selected.value);
+  rolledNumbers.value[newRoll]++;
+  numberOfRolls.value++;
+};
 </script>
 
 <style>
-  /* css goes here */
+/* css goes here */
 </style>
